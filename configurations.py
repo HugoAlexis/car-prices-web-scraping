b2c23e57384e3a3211d5353146f80ec3a5152a6f
hugoalexis <hugoalexistorrespasillas@gmail.com>
2025-04-11 15:33:32 -0600

    Add python script for configuration options

---
+++ configurations.py
@@ -0,0 +1,5 @@
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'es-MX,en-US,en;q=0.5',
+}
