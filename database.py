817cbbd5dc6abd57fdb5f0112695b3e7ccd4f800
hugoalexis <hugoalexistorrespasillas@gmail.com>
2025-04-12 17:23:34 -0600

    Database module changed to allow using postgres

--- database.py
+++ database.py
@@ -1,8 +1,15 @@
 import sqlite3
+import psycopg2
 import os
+from dotenv import load_dotenv
+load_dotenv('.env')
 
+DATABASE_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
 
-DATABASE_NAME = 'database.db'
 with open('database_squema.sql', 'r') as sql_file:
     DATABASE_SCHEMA = sql_file.read()
 
@@ -27,7 +34,7 @@ class Database:
         else:
             return cls._instance
 
-    def __init__(self):
+    def __init__(self, use_postgres=False):
         """
         If the database instance (singleton) is instantiated, it creates the database
         connection and cursor. If the database file doesn't exist already, create the
@@ -35,12 +42,69 @@ class Database:
         """
         if self.__initialized:
             return
-        db_exists = os.path.exists(DATABASE_NAME)
-        self._connection = sqlite3.connect(DATABASE_NAME)
+
+        # Initialize chosen database (postgres or sqlite)
+        self._initialized = False
+        if use_postgres:
+            self._initialize_db_postgres()
+            self.use_postgres = True
+        if not self._initialized:
+            self._initialize_db_sqlite()
+            self.use_postgres = False
+
+    def _initialize_db_sqlite(self):
+        """
+        Initializes the SQLite database, connection and cursor.
+        Creates the database if it doesn't already exist.'
+
+        :return: None
+        """
+        db_fullname = f'{DATABASE_NAME}.sqlite'
+        db_exists = os.path.exists(db_fullname)
+        self._connection = sqlite3.connect(db_fullname)
         self._cursor = self._connection.cursor()
         if not db_exists:
             self._create_schema()
         self._initialized = True
+        print('Using DB SQLite')
+
+    def _initialize_db_postgres(self):
+        """
+        Initializes the PostgreSQL database, connection and cursor.
+        Creates the database if it doesn't already exist.'
+
+        :return: None
+        :return:
+        """
+        try:
+            conn = psycopg2.connect(
+                dbname='postgres',
+                user=DB_USER,
+                password=DB_PASSWORD,
+                host=DB_HOST,
+                port=DB_PORT
+            )
+            conn.autocommit = True
+            with conn.cursor() as cursor:
+                cursor.execute(f'SELECT 1 FROM pg_database WHERE datname ILIKE \'{DATABASE_NAME}\';')
+                already_exists = len(cursor.fetchall()) > 0
+                if not already_exists:
+                    cursor.execute(f'CREATE DATABASE {DATABASE_NAME};')
+                    print('PostgreSQL Database Created')
+            conn.close()
+        except psycopg2.OperationalError:
+            print('PostgreSQL connection failed. Using SQLite instead')
+            return
+        self._connection = psycopg2.connect(
+            dbname=DATABASE_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT
+        )
+        self._cursor = self._connection.cursor()
+        self._initialized = True
+
 
     def query(self, sql, params):
         self.cursor.execute(sql, params)
@@ -49,6 +113,28 @@ class Database:
     def select_query(self, query):
         res = self.cursor.execute(query)
         return res.fetchall()
+
+    def select(self, table, columns='*', where_clause=None, where_params=None):
+        """
+           Executes a safe SELECT query on the database.
+
+           Args:
+               table (str): Name of the table.
+               columns (list): List of columns to select.
+               where_clause (str, optional): WHERE condition with placeholders.
+               where_params (tuple or list, optional): Parameters for the WHERE clause.
+
+           Returns:
+               list: Query results.
+           """
+        cols = ', '.join(columns)
+        query = f"SELECT {cols} FROM {table}"
+
+        if where_clause:
+            query += f" WHERE {where_clause}"
+
+        self.cursor.execute(query, where_params or [])
+        return self.cursor.fetchall()
 
 
     @property
@@ -71,10 +157,5 @@ class Database:
 
 
 if __name__ == '__main__':
-    db = Database()
-    db.cursor.execute(
-        'INSERT INTO car_item VALUES (?, ?, ?, ?, ?)',
-        (1, 'algo.com', 'ford', 'explorer', 2019)
-    )
-    db._connection.commit()
+    db = Database(use_postgres=True)
 
