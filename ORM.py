3341f03953b84c199fb095a148480573844de00d
hugoalexis <hugoalexistorrespasillas@gmail.com>
2025-04-12 19:29:02 -0600

    Add Object Model for version_details table (VersionDetails)

--- ORM.py
+++ ORM.py
@@ -1,4 +1,7 @@
 import time
+
+from setuptools.command.build_ext import if_dl
+
 from database import Database
 
 db = Database(use_postgres=True)
@@ -97,6 +100,97 @@ class Version(ObjectModelMixin):
         if not self._already_exists:
             super().dump()
 
+
+class VersionDetails(ObjectModelMixin):
+    table_name = 'version_details'
+    table_id = 'version_id'
+
+    def __init__(
+            self,
+            version_object,
+            mileage=None,
+            cylinders=None,
+            num_of_gears=None,
+            fuel_range=None,
+            engine_type=None,
+            fuel_type=None,
+            horsepower=None,
+            rim_inches=None,
+            rim_material=None,
+            num_of_doors=None,
+            num_of_passengers=None,
+            num_of_airbags=None,
+            has_abs=None,
+            interior_materials=None,
+            has_start_button=None,
+            has_cruise_control=None,
+            has_distance_sensor=None,
+            has_bluetooth=None,
+            has_rain_sensor=None,
+            has_automatic_emergency_breaking=None,
+            has_gps=None,
+            has_sunroof=None,
+            has_androidauto=None,
+            length_meters=None,
+            height_meters=None,
+            width_meters=None,
+            weigth_kg=None
+        ):
+        self._version_object = version_object
+        self.version_id = version_object.version_id
+        self.mileage = round(mileage, 1) if mileage else None
+        self.cylinders = int(cylinders) if cylinders else None
+        self.num_of_gears = int(num_of_gears) if num_of_gears else None
+        self.fuel_range = int(fuel_range) if fuel_range else None
+        self.engine_type = engine_type.capitalize() if engine_type else None
+        self.fuel_type = fuel_type.capitalize() if fuel_type else None
+        self.horsepower = int(horsepower) if horsepower else None
+        self.rim_inches = int(rim_inches) if rim_inches else None
+        self.rim_material = rim_material.capitalize() if rim_material else None
+        self.num_of_doors = int(num_of_doors) if num_of_doors else None
+        self.num_of_passengers = int(num_of_passengers) if num_of_passengers else None
+        self.num_of_airbags = int(num_of_airbags) if num_of_airbags else None
+        self.has_abs = bool(has_abs) if has_abs else None
+        self.interior_materials = interior_materials.capitalize() if interior_materials else None
+        self.has_start_button = bool(has_start_button) if has_start_button else None
+        self.has_cruise_control = bool(has_cruise_control) if has_cruise_control else None
+        self.has_distance_sensor = bool(has_distance_sensor) if has_distance_sensor else None
+        self.has_bluetooth = bool(has_bluetooth) if has_bluetooth else None
+        self.has_rain_sensor = bool(has_rain_sensor) if has_rain_sensor else None
+        self.has_automatic_emergency_breaking = bool(has_automatic_emergency_breaking) if has_automatic_emergency_breaking else None
+        self.has_gps = bool(has_gps) if has_gps else None
+        self.has_sunroof = bool(has_sunroof) if has_sunroof else None
+        self.has_androidauto = bool(has_androidauto) if has_androidauto else None
+        self.length_meters = int(length_meters) if length_meters else None
+        self.height_meters = int(height_meters) if height_meters else None
+        self.width_meters = int(width_meters) if width_meters else None
+        self.weight_kg = int(weigth_kg) if weigth_kg else None
+
+version = Version(
+    'Chevrolet',
+    'Aveo',
+    'advanced',
+    2020,
+    'hatchback',
+    1.4,
+    'Automatic'
+)
+
+version_details = VersionDetails(
+    version_object=version,
+    mileage=15,
+    cylinders=5,
+    num_of_gears=4,
+    fuel_range=10,
+    engine_type='Combustion',
+    fuel_type='Gasoline',
+    horsepower=123,
+    rim_inches=14,
+    rim_material='Aluminium'
+)
+
+version.dump()
+version_details.dump()
 
 
 if __name__ == '__main__':
