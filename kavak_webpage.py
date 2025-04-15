da717f8c820b71135795fd06878bceb9f38faa31
hugoalexis <hugoalexistorrespasillas@gmail.com>
2025-04-14 21:03:33 -0600

    Create webpage_models module to model webpage to parse from html to structured data

--- kavak_webpage.py
+++ kavak_webpage.py
@@ -1,213 +1,9 @@
-import re
 import requests
 from bs4 import BeautifulSoup
-from scraping import Scraper, PageIterator, CarItem
-from requests.exceptions import RequestException
+from scraping import Scraper, PageIterator
+from webpage_models import KavakItem
+import time
 
-
-class KavakItemScraper(Scraper):
-    """
-    Implementation for scraping and retrieving information about vehicles listed on Kavak.
-    """
-    def __init__(self, url):
-        """
-        Given the listing url of the Kavak car, makes a request to get the html from the
-        car listing to extract information about the vehicle listed.
-        :param url: URL of the Kavak car.
-        """
-        super().__init__(url)
-
-    @property
-    def item_id(self):
-        """Id of the listed car item."""
-        text = self._scrape_sibling('Stock ID')
-        try:
-            return int(text)
-        except ValueError:
-            return None
-
-    @property
-    def brand(self):
-        """Brand of the listed car item"""
-        text = self._scrape_css_selector('ul.breadcrumb_breadcrumb__nPwIW li:nth-child(2) a')
-        return text.capitalize()
-
-    @property
-    def model(self):
-        """Model of the listed car item"""
-        text = self._scrape_css_selector('ul.breadcrumb_breadcrumb__nPwIW li:nth-child(3) a')
-        return text.capitalize()
-
-    @property
-    def year(self):
-        """Year of the listed car item"""
-        text = self._scrape_css_selector('ul.breadcrumb_breadcrumb__nPwIW li:nth-child(4) a')
-        return int(text)
-
-    @property
-    def engine_displacement(self):
-        """Engine displacement of the listed car item"""
-        disp_pattern = re.compile(r'(\d\.\d).*')
-        text = self._scrape_css_selector('aside.buy-box_wrapper__jCjj4 h1.header_title__l7xVU') or ''
-        displacement = disp_pattern.findall(text)
-        if displacement:
-            displacement = displacement[0]
-            return float(displacement)
-        return None
-
-    @property
-    def version(self):
-        """Version of the listed car item"""
-        text = self._scrape_css_selector('ul.breadcrumb_breadcrumb__nPwIW li:nth-child(5)')
-        # Remove engine displacement from the text version
-        if text:
-            text_version = re.compile(r'\d+\.\d+\s(.*)').findall(text)
-            if text_version:
-                return text_version[0].capitalize()
-        return None
-
-    @property
-    def body_style(self):
-        """Body style of the listed car item"""
-        text = self._scrape_sibling('Tipo de Carrocer')
-        if text:
-            return text.capitalize()
-
-    @property
-    def fuel_economy(self):
-        """Fuel eco of the listed car item"""
-        text = self._scrape_sibling('Consumo combinado')
-        if text:
-            return text.capitalize()
-
-    @property
-    def odometer(self):
-        """Odometer of the listed car item"""
-        text = self._scrape_css_selector('p.header_subtitle__y_nvg')
-        if text:
-            odometer_pattern = re.compile(r'([0-9,]+)\skm.*')
-            odometer = odometer_pattern.findall(text)
-            if odometer:
-                return int(odometer[0].replace(',', ''))
-
-    @property
-    def transmission(self):
-        """Type of transmission of the listed car item"""
-        text = self._scrape_css_selector('aside .buy-box_extended__YcHd4 ul li:nth-child(3) .filters_info__WlDer span:nth-child(2)')
-        if text:
-            return text.strip().capitalize()
-
-
-
-    @property
-    def city(self):
-        """City of the listed car item"""
-        text = self._scrape_sibling('Ciudad$')
-        if text:
-            return text.capitalize()
-
-    @property
-    def cylinders(self):
-        """Cylinders of the listed car item"""
-        text = self._scrape_sibling('Cilindros')
-        if text:
-            return int(text)
-
-    @property
-    def number_of_gears(self):
-        """Number of gears of the listed car item"""
-        text = self._scrape_sibling('N.?mero de Velocidades')
-        if text:
-            return int(text)
-
-    @property
-    def horsepower(self):
-        """Horsepower of the listed car item"""
-        text = self._scrape_sibling('Caballos de Fuerza')
-        if text:
-            return int(text)
-
-    @property
-    def doors(self):
-        """Doors of the listed car item"""
-        text = self._scrape_sibling('N.?mero de Puertas')
-        if text:
-            return int(text)
-
-    @property
-    def cruise_control(self):
-        """Wheather the listed car item has cruise control"""
-        text = self._scrape_sibling('Control de Crucero')
-        if text:
-            if text.capitalize() in ['Sí', 'Si']:
-                return True
-            return False
-        return False
-
-    @property
-    def distance_sensor(self):
-        """Weather the listed car item has distance sensor"""
-        text = self._scrape_sibling('Sensor de distancia')
-        if text:
-            if text.capitalize() in ['Sí', 'Si']:
-                return True
-            return False
-        return False
-
-    @property
-    def start_button(self):
-        """Weather the listed car item has start button"""
-        text = self._scrape_sibling('Boton de Encendido')
-        if text:
-            if text.capitalize() in ['Sí', 'Si']:
-                return True
-            return False
-        return False
-
-    @property
-    def number_of_airbags(self):
-        """Number of airbags of the listed car item"""
-        text = self._scrape_sibling('N.?mero total de Airbags')
-        if text:
-            return int(text)
-        return False
-
-    @property
-    def abs(self):
-        """Weather the  listed car item has ABS system"""
-        text = self._scrape_sibling('Tipo Frenos ABS')
-        if text:
-            if text.capitalize() in ['Sí', 'Si']:
-                return True
-            return False
-        return False
-
-    @property
-    def passengers(self):
-        """Number of passengers of the listed car item"""
-        text = self._scrape_sibling('N.?mero de Pasajeros')
-        if text:
-            return int(text)
-
-    @property
-    def interior_materials(self):
-        """Material used for the interior in the listed car item"""
-        text = self._scrape_sibling('Material Asientos')
-        if text:
-            return text.capitalize()
-
-    @property
-    def price_without_discount(self):
-        """Price of the listed car item without discount"""
-        text = self._scrape_css_selector('.price_amount__dRxZ8')
-        if text:
-            return int(text.replace(',', '').replace('$', ''))
-
-    @property
-    def price(self):
-        text = self._scrape_css_selector('span.amount_uki-amount__extraLarge__price__ZMOLc')
-        if text:
-            return int(text.replace(',', '').replace('$', ''))
 
 class KavakPageIterator(PageIterator):
     """
@@ -236,10 +32,12 @@ class KavakPageIterator(PageIterator):
             BeautifulSoup(req.content, 'html.parser')
             .select('a.results_results__pagination-nav__Qcftr')
         )
+
         if len(pagination_buttons) < 2 and self.next_iteration > 1:
             raise StopIteration
         if req.status_code != 200:
             raise StopIteration
+
         return KavakPageScraper(req.url)
 
 
@@ -249,7 +47,7 @@ class KavakPageScraper(Scraper):
     (from pages of the website's pagination).
     """
 
-    def get_items(self):
+    def url_all_items(self):
         """
         Return a list of CarIte instances found on the pagination page.
 
@@ -261,36 +59,30 @@ class KavakPageScraper(Scraper):
             found_many='first',
         ).children
 
-        all_car_items = []
+        dict_all_items = {}
         for item in all_items:
-            if item.find(string=re.compile('Vende tu auto y.*')):
+            if 'Vende tu auto' in item.text:
                 continue
-            else:
-                car_item = self._div_to_car_item(item)
-                all_car_items.append(car_item)
-        return all_car_items
-
-    @staticmethod
-    def _div_to_car_item(item):
-        """
-        Given a (BeautifulSoup) tag containing the listing of a car item,
-        return a CarItem instance.
-        :param item: BeautifulSoup tag containing the listing of a car item
-        :return CarItem instance: Instance containing the information of the car listing.
-        """
-        car_id = int(item.a.attrs['data-testid'].split('-')[-1])
-        url = item.a['href']
-        try:
-            price = int(item.find(
-                'span', {'class': 'amount_uki-amount__large__price__2NvVx'}
-            ).text.replace(',', ''))
-            status = 'Disponible'
-        except AttributeError:
-            price = None
-            status = 'Apartado'
-
-        return CarItem(car_id, url, price, status)
+            url = item.a.attrs['href']
+            id = item.a.attrs['data-testid'].split('-')[-1]
+            dict_all_items[id] = url
+        return dict_all_items
 
 
     def __str__(self):
         return self.url
+
+
+if __name__ == '__main__':
+    kavak_iterator = KavakPageIterator('https://www.kavak.com/mx/seminuevos')
+    for page in kavak_iterator:
+        items = page.url_all_items()
+        for id, url in items.items():
+            item = KavakItem(url)
+            print('Brand: {}'.format(item.brand))
+            print('Model: {}'.format(item.model))
+            print('Original_price: {}'.format(item.original_price))
+            print('Price: {}'.format(item.price))
+            print('Report: {}'.format(item.url_report))
+            time.sleep(3)
+        break
---
+++ webpage_models.py
@@ -0,0 +1,176 @@
+from scraping import Scraper
+
+
+class KavakItem(Scraper):
+    @Scraper.css_extract_text()
+    def original_price(self): return 'span.price_amount__dRxZ8'
+
+    @Scraper.css_extract_text()
+    def price(self): return 'span.amount_uki-amount__extraLarge__price__ZMOLc'
+
+    @Scraper.css_extract_text()
+    def brand(self): return 'ul.breadcrumb_breadcrumb__nPwIW li:nth-child(2) a'
+
+    @Scraper.css_extract_text()
+    def model(self): return 'ul.breadcrumb_breadcrumb__nPwIW li:nth-child(3) a'
+
+    @Scraper.css_extract_text()
+    def year_prod(self): return 'ul.breadcrumb_breadcrumb__nPwIW li:nth-child(4) a'
+
+    @Scraper.css_extract_text()
+    def version(self): return 'ul.breadcrumb_breadcrumb__nPwIW li:nth-child(5) span'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def engine_displacement(self): return '(\d+(?:\.\d+)?)\s?Litros'
+
+    @Scraper.re_extract_text()
+    def cylinders(self): return '(\d+)\s?Cilindros'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def mileage(self): return r'(\d+\.\d+)\s?Consumo'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def city(self): return r'([a-zA-Z\sáéíóú]+)\s?Ciudad'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def car_id(self): return r'(\d+)\s?Stock ID'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def horsepower(self): return r'(\d+)\s?Caballos de Fuerza'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def weight_kg(self): return r'(\d+)\s?Peso bruto'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def fuel_range(self): return r'(\d+)\s?Autonom.a combinada'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def num_of_gears(self): return r'(\d+)\s?N.mero de Velocidades'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def fuel_type(self): return r'([A-Z]{1}[a-z]+)\s?Combustible'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def engine_type(self): return r'([A-ZÁÉÍÓÚ][a-záéíóú]+)\s?Tipo de motor'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def num_of_doors(self): return r'(\d+)\s?N.mero de Puertas'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def num_of_airbags(self): return r'(\d+)N.mero.*Airbags'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def rim_inches(self): return r'(\d+)\s?Di.metro de Rin'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def rim_material(self): return r'([A-ZÁÉÍÓÚ][a-záéíóú]+)\s?Tipo de Rin'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def body_style(self): return r'([A-ZÁÉÍÓÚ][a-záéíóú]+)\s?Tipo de Carrocer.a'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def has_startstop_button(self): return r'([A-ZÁÉÍÓÚa-záéíóú]{2})Start.Stop'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def has_gps(self): return r'([A-ZÁÉÍÓÚa-záéíóú]{2})\s?GPS'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def has_start_button(self): return r'([A-ZÁÉÍÓÚa-záéíóú]{2})\s?Boton de Encendido'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def has_sunroof(self): return r'([A-ZÁÉÍÓÚa-záéíóú]{2})\s?Techo Panor.mico'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def has_cruise_control(self): return r'([A-ZÁÉÍÓÚa-záéíóú]{2})\s?Control de Crucero'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def has_heated_seats(self): return r'([A-ZÁÉÍÓÚa-záéíóú]{2})\s?Asientos.*alefaccionados'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def has_distance_sensor(self): return r'([A-ZÁÉÍÓÚa-záéíóú]{2})\s?Sensor de distancia'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def has_abs(self): return r'([A-ZÁÉÍÓÚa-záéíóú]{2})\s?Tipo Frenos ABS'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def has_rain_sensor(self): return r'([A-ZÁÉÍÓÚa-záéíóú]{2})\s?Sensor de lluvia'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def has_automatic_emergency_breaking(self):
+        return r'([A-ZÁÉÍÓÚa-záéíóú]{2})\s?Asistencia de frenado'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def has_bluetooth(self): return r'([A-ZÁÉÍÓÚa-záéíóú]{2})\s?Bluetooth'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def num_of_passengers(self): return r'(\d+)\s?N.mero de Pasajeros'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def interior_materials(self): return r'([A-ZÁÉÍÓÚ][a-záéíóú]+)\sMaterial Asientos?'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def has_touchscreen(self): return r'([A-ZÁÉÍÓÚa-záéíóú]{2})\s?Pantalla T.ctil'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def has_androidauto(self): return r'([A-ZÁÉÍÓÚa-záéíóú]{2})\s?Android Auto'
+
+    @Scraper.re_extract_text(outer_tag='div.desktop_car-detail__start__BToHy')
+    def has_androidauto(self): return r'([A-ZÁÉÍÓÚa-záéíóú]{2})\s?Apple CarPlay'
+
+
+    @Scraper.re_extract_text(outer_tag='aside.buy-box_wrapper__jCjj4 ')
+    def transmission_type(self): return r'Transmisión\s?(Automático|Manual)'
+
+
+
+    @property
+    def url_image(self):
+        img_tag = self.soup.select('div.keen-slider__slide img')
+        if img_tag:
+            return  img_tag[0].attrs['src']
+        return None
+
+
+class AutocosmosItem(Scraper):
+
+    @property
+    def combustible(self):
+        return self._scrape_sibling('Combustible', 'td')
+
+    @property
+    def has_sunroof(self):
+        has_x = self._scrape_sibling('Quemacocos', 'td') or ''
+        return False if ('no tiene' in has_x.strip().lower()) else True
+
+
+if __name__ == '__main__':
+    item = KavakItem('https://www.kavak.com/mx/usado/nissan-frontier-25_platinum_le_auto-pickup-2022')
+    print('brand: ', item.brand)
+    print('model: ', item.model)
+    print('year: ', item.year_prod)
+    print('transmission_type: ', item.transmission_type)
+    print('mileage: ', item.mileage)
+    print('cylinders: ', item.cylinders)
+    print('num_of_gears:', item.num_of_gears)
+    print('fuel_type: ', item.fuel_type)
+    print('fuel_range: ', item.fuel_range)
+    print('engine_type: ', item.engine_type)
+    print('horsepower: ', item.horsepower)
+    print('rim_inches: ', item.rim_inches)
+    print('rim_materials: ', item.rim_material)
+    print('num_of_doors: ', item.num_of_doors)
+    print('num_of_passengers: ', item.num_of_passengers)
+    print('num_of_airbags: ', item.num_of_airbags)
+    print('has_abs: ', item.has_abs or 'No')
+    print('interior_materials: ', item.interior_materials or 'No')
+    print('has_startstop_button: ', item.has_startstop_button or 'No')
+    print('has_cruise_contro: ', item.has_cruise_control or 'No')
+    print('has_distance_sensor: ', item.has_distance_sensor or 'No')
+    print('has_bluetooth: ', item.has_bluetooth or 'No')
+    print('has_rain_sensor: ', item.has_rain_sensor or 'No')
+    print('has_emergency_breaking: ', item.has_automatic_emergency_breaking or 'No')
+    print('has_gps: ', item.has_gps or 'No')
+    print('has_sunroof: ', item.has_sunroof or 'No')
+    print('has_androidauto: ', item.has_androidauto or 'No')
+    print('weigth_kg: ', item.weight_kg)
+    print(item.url_image)
